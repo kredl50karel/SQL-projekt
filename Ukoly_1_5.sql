e36612855a8b56d5f8ddcbb0011756f1bffc8b1c
Karel Kredl <85839350+kredl50karel@users.noreply.github.com>
2022-06-27 01:17:56 +0200

    Add files via upload

---
+++ Ukoly_1_5.sql
@@ -0,0 +1,30 @@
+WITH tab1 AS (
+	SELECT DISTINCT 
+		rok, 
+		prumerna_vyplata, 
+		odvetvi,
+		industry_branch_code
+     FROM t_Karel_Kredl_project_SQL_primary_final	
+	), 
+tab2 AS (
+	SELECT DISTINCT 
+		rok, 
+		prumerna_vyplata, 		
+		odvetvi,
+		industry_branch_code
+     FROM t_Karel_Kredl_project_SQL_primary_final	
+	)
+SELECT 
+	tab1.rok AS rok1, 
+	tab1.odvetvi,
+    tab1.industry_branch_code,	
+    tab1.prumerna_vyplata, 
+	tab2.rok AS rok2, 
+	tab2.prumerna_vyplata, 
+	ROUND((tab2.prumerna_vyplata - tab1.prumerna_vyplata) / tab1.prumerna_vyplata * 100,2) AS rozdil_procenta
+FROM tab1
+JOIN tab2
+	ON tab1.industry_branch_code = tab2.industry_branch_code
+    AND tab1.rok = tab2.rok - 1
+ ORDER BY rozdil_procenta, tab1.odvetvi, rok1;
+
